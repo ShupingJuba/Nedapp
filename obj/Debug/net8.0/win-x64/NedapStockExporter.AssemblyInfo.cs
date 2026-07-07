e4c6c47ba79a4e9dabdbefca5e01eabd53e7c93b
ShupingJuba <50945462+ShupingJuba@users.noreply.github.com>
2026-07-07 13:59:19 +0200

    changes not working properly

--- obj/Debug/net8.0/win-x64/NedapStockExporter.AssemblyInfo.cs
+++ obj/Debug/net8.0/win-x64/NedapStockExporter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NedapStockExporter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6c2c6cb744e9d4df78dab3fcffb330596aa6f86")]
 [assembly: System.Reflection.AssemblyProductAttribute("NedapStockExporter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NedapStockExporter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
